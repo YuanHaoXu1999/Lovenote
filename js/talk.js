340313da91b57f04ebae92761c33c92488442b1a
YuanHaoXu1999 <Yuanhao.xu@student.ap.be>
2025-03-22 15:09:41 +0100

    first commit

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#xyhjzr", {
+    loop: true,
+    cursorSpeed: 1000,
+    speed: 100
+})
+    .type("XYH && JZR")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("徐元浩❤姜止柔")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkToXHJ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
